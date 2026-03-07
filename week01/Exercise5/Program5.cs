a40a799bdb2f85bb181f8c8625a65ddbf5911843
SebasMagno1 <114171307+SebasMagno1@users.noreply.github.com>
2026-03-07 14:36:49 -0700

    Add files via upload

---
+++ week01/Exercise5/Program5.cs
@@ -0,0 +1,39 @@
+using System;
+
+class Program
+{
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+    }
+
+    static void Main()
+    {
+        DisplayWelcome();                      
+        string userName = PromptUserName();    
+        int favoriteNumber = PromptUserNumber(); 
+        int squared = SquareNumber(favoriteNumber); 
+        DisplayResult(userName, squared);      
+    }
+}
